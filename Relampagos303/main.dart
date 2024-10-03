d06dee0a27f8de95365288cf927eca0ebc19ffa3
Sherwin Relampagos <sherwinblack21@gmail.com>
2024-10-03 11:37:18 +0800

    Add files via upload

---
+++ Relampagos303/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: (){
+        print('MyButton Was Tapped');
+      },
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.black,
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+    
+  }
+}
+
+void main ()
+{
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(
+
+          ),
+        ),
+      ),
+    )
+  );
+}
